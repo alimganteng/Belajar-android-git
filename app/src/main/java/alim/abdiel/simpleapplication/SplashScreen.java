f6581a56a466fca5b94cc44866fa282d5772f0d4
Abdiel Alim <you@example.com>
2018-10-05 07:55:05 +0700

    initiate project

---
+++ app/src/main/java/alim/abdiel/simpleapplication/SplashScreen.java
@@ -0,0 +1,32 @@
+package alim.abdiel.simpleapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.preference.Preference;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.prefs.Preferences;
+
+import alim.abdiel.simpleapplication.util.PreferencesHelper;
+
+public class SplashScreen extends AppCompatActivity {
+    PreferencesHelper instance
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        instance = PreferencesHelper.getInstance (getApplicationContext());
+        int splashInterval = 10;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            if (!instance.isLogin)()) {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+            } else {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, splashInterval);
+    }
+}
